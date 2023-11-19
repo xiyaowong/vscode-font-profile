a2f1dd21940c48b9bae808a0c6ce614223f09aed
wongxy <xiyao.wong@foxmail.com>
2023-11-20 00:17:10 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Font Profile
+
+<a href="https://marketplace.visualstudio.com/items?itemName=xiyaowong.font-profile" target="__blank"><img src="https://img.shields.io/visual-studio-marketplace/v/xiyaowong.font-profile.svg?color=eee&amp;label=VS%20Code%20Marketplace&logo=visual-studio-code" alt="Visual Studio Marketplace Version" /></a>
+
+A mini plugin used for managing font configurations.
+
+![demo](https://github.com/xiyaowong/vscode-font-profile/assets/47070852/7868b0f9-8e6a-4e3d-a1a9-5bc393fe8128)
+
+## Configrations
+
+- `font-profile.profile`: The profile to use.
+- `font-profile.profiles`: Your font profiles.
+
+## Commands
+
+- `font-profile.switch`: Switch the font profile
+
+## LICENSE
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { commands, window, workspace } from 'vscode'
+import type { ConfigurationChangeEvent, ExtensionContext } from 'vscode'
+
+const EXT_ID = 'font-profile'
+const CONTROLLED_ITEMS = ['editor.fontFamily', 'editor.fontLigatures', 'editor.fontSize', 'editor.fontWeight', 'editor.fontVariations']
+
+interface Profile { [key: string]: any }
+
+function setupProfiles(e?: ConfigurationChangeEvent) {
+  if (!e?.affectsConfiguration(EXT_ID)) return
+
+  const extConfig = workspace.getConfiguration(EXT_ID)
+  const allConfig = workspace.getConfiguration()
+  // First time initialization, setup the default profile
+  const profileName = extConfig.get<string>('profile')
+  const profiles = extConfig.get<Profile>('profiles', {})
+  if (!Object.keys(profiles).length) {
+    const defaultProfile: Profile = {}
+    CONTROLLED_ITEMS.forEach((key) => {
+      defaultProfile[key] = allConfig.get(key)
+    })
+    if (profileName !== 'default')
+      extConfig.update('profile', 'default', true)
+    extConfig.update('profiles', { default: defaultProfile }, true)
+    window.showInformationMessage('The default Font Profile initialized')
+    return
+  }
+  // Check if the current profile is valid
+  if (!profileName || !profiles[profileName]) {
+    window.showErrorMessage(`Invalid profile: ${profileName}`)
+    return
+  }
+  const profile = profiles[profileName]
+  // check if any of the controlled items is changed and update it
+  CONTROLLED_ITEMS.forEach((key) => {
+    const preferredValue = profile[key]
+    if (preferredValue != null && preferredValue !== allConfig.get(key)) {
+      allConfig.update(key, preferredValue, true)
+    }
+  })
+}
+
+export function activate(ext: ExtensionContext) {
+  setupProfiles()
+  ext.subscriptions.push(
+    workspace.onDidChangeConfiguration(setupProfiles),
+    commands.registerCommand(`${EXT_ID}.switch`, () => {
+      const extConfig = workspace.getConfiguration(EXT_ID)
+      const profiles = extConfig.get<Profile>('profiles', {})
+      const names = Object.keys(profiles)
+      if (!names.length) {
+        window.showErrorMessage('No profiles found')
+        return
+      }
+      window.showQuickPick(names).then((name) => {
+        if (name) {
+          extConfig.update('profile', name, true)
+        }
+      })
+    }))
+}
+
+export function deactivate() {
+}
